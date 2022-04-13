3cf38e20e5890989f8c7f1436bfb75e7e15ff1d7
SoponwichT <aot_aln32@hotmail.com>
2022-04-13 20:31:40 +0700

    Show data from GET API

--- pages/index.js
+++ pages/index.js
@@ -3,43 +3,38 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import { QuoteCard } from '../components/quotecard'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const API_URL = "https://allinquote-server.herokuapp.com/quotes";
+
+  const getQuotes = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const responsedata = await response.json();
+      setQuotes(responsedata);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getQuotes();
+  }, [])
+
+  const quoteData = quotes.data;
+  const quoteElements = quoteData?.map(data => {
+    return <QuoteCard quote={data}/>
+  });
 
   return (
     <>
       <div className='quote-container flex flex-row max-w-7xl mx-auto gap-y-24 gap-x-12 flex-wrap justify-left'>
-        <QuoteCard quote={{
-          text: '"In summers, after 1 hour of extreme gaming you can use your laptop to iron your shirt."',
-          author: "Your name",
-          color: 0,
-          category: "work"
-        }} />
-        <QuoteCard quote={{
-          text: '"In summers, after 1 hour of extreme gaming you can use your laptop to iron your shirt."',
-          author: "Your name",
-          color: 1,
-          category: "work"
-        }} />
-        <QuoteCard quote={{
-          text: '"In summers, after 1 hour of extreme gaming you can use your laptop to iron your shirt."',
-          author: "Your name",
-          color: 2,
-          category: "work"
-        }} />
-        <QuoteCard quote={{
-          text: '"In summers, after 1 hour of extreme gaming you can use your laptop to iron your shirt."',
-          author: "Your name",
-          color: 3,
-          category: "work"
-        }} />
-        <QuoteCard quote={{
-          text: '"In summers, after 1 hour of extreme gaming you can use your laptop to iron your shirt."',
-          author: "Your name",
-          color: 3,
-          category: "work"
-        }} />
-        
+        {quoteElements}
       </div>
     </>
   )
